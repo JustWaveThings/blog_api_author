24449d3faaba29af8510369f717cdf5de5249d07
JustWaveThings <justwavethings@proton.me>
2023-09-29 15:09:20 -0400

    Create container for editor

---
+++ src/components/PEContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PostEditor from './PostEditor';
+function PEContainer() {
+  return (
+    <Fragment>
+      <div>PEContainer</div>
+      <PostEditor />
+    </Fragment>
+  );
+}
+
+export default PEContainer;
