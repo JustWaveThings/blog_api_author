2ab9ac1f273ea780ec2ff93840378b931fda5969
JustWaveThings <justwavethings@proton.me>
2023-10-21 12:56:54 -0400

    fix: update fetch to get author route

--- src/loaders/homeLoader.js
+++ src/loaders/homeLoader.js
@@ -1,5 +1,5 @@
 async function homeLoader() {
-  return await fetch('http://localhost:3000/posts/').then(res => res.json());
+  return await fetch('http://localhost:3000/author/').then(res => res.json());
 }
 
 export default homeLoader;
