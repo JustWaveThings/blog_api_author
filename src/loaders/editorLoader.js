0295fa10039e248bb899dd6978f7d7b2e125f340
JustWaveThings <justwavethings@proton.me>
2023-10-26 12:54:56 -0400

    Add validator pkg and unescape user input from db

--- src/loaders/editorLoader.js
+++ src/loaders/editorLoader.js
@@ -1,7 +1,12 @@
+import validator from 'validator';
+
 async function editorLoader(req) {
   try {
     const res = await fetch(`http://localhost:3000/author/${req.params.id}`);
     const data = await res.json();
+    data.body = validator.unescape(data.body);
+    data.title = validator.unescape(data.title);
+    data.subtitle = validator.unescape(data.subtitle);
     return data;
   } catch (error) {
     console.error(error);
