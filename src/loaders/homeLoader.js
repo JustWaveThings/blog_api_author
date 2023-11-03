abb13e16374f1cdf12f5de6ef537d951ba4de95a
JustWaveThings <justwavethings@proton.me>
2023-11-03 19:45:01 -0400

    bug: currently working fetch config

--- src/loaders/homeLoader.js
+++ src/loaders/homeLoader.js
@@ -1,10 +1,21 @@
 async function homeLoader() {
   try {
-    const response = await fetch('http://localhost:3000/author/');
+    const response = await fetch('http://localhost:3000/author/', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        mode: 'cors',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    });
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    return await response.json();
+
+    const data = await response.json();
+    console.log(data);
+    return data;
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
     return { error: error.message };
