1757f0ece04766ef6e0032e3525314793a0544a9
JustWaveThings <justwavethings@proton.me>
2023-11-16 11:57:47 -0500

    refactor: DRY - combine fetchall and fetch one fetch function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
 import validator from 'validator'; // used by editorFetch
 
-// this file is used to make api calls to the backend using fetch
-
-export async function allPostFetch() {
+export async function postFetch(id) {
+  const url = id
+    ? `http://localhost:3000/author/${id}`
+    : 'http://localhost:3000/author/';
   try {
-    const response = await fetch('http://localhost:3000/author/', {
+    const res = await fetch(url, {
       method: 'GET',
       credentials: 'include',
       headers: {
@@ -12,28 +13,18 @@ export async function allPostFetch() {
         mode: 'cors',
       },
     });
-    if (!response.ok) {
-      throw new Error(response.statusText);
+    if (!res.ok) {
+      throw {
+        message: 'Failed to fetch post(s)',
+        statusText: res.statusText,
+        status: res.status,
+      };
     }
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const data = await res.json();
+    return data.posts;
   } catch (error) {
-    console.log(error);
+    return error;
   }
-}
-
-export async function postFetch(params) {
-  const res = await fetch(`http://localhost:3000/author/${params.id}`, {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      mode: 'cors',
-    },
-  });
-  const data = await res.json();
-  return data;
 }
 
 export async function editorFetch(params) {
