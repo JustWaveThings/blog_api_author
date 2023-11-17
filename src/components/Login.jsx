6d2b8f269ac2aaf312f226bb21d3b675a3868350
JustWaveThings <justwavethings@proton.me>
2023-11-17 11:09:47 -0500

    refactor: login form to use Form component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,35 @@
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Form } from 'react-router-dom';
 import login from '../utils/login';
 
 function Login() {
-  const [loginData, setLoginData] = useState({ username: '', password: '' });
-  const [loginState, setLoginState] = useState(false);
-  const handleInputChange = e => {
-    const { name, value } = e.target;
-    setLoginData(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const { response, username } = await login(loginData);
-
-    if (response.ok) {
-      console.log(
-        `${response.status} http code. ${username} should be logged into the frontend author dashboard`
-      );
-    } else {
-      console.log(
-        `${response.status} http code. This should show that we are not logged in on the FE`
-      );
-    }
-  }
-
   return (
     <div className='element'>
       <h1>Blog Author Dashboard</h1>
-      {loginState && <h2> user is logged in </h2>}
+
       <p>
         Login to create, edit, delete, publish and unpublish blog posts, as well
         as remove comments.
       </p>
-      <form onSubmit={e => handleSubmit(e)}>
-        <label>
-          Username:
-          <input
-            type='text'
-            name='username'
-            value={loginData.username}
-            onChange={handleInputChange}
-            autoComplete='username'
-          />
-        </label>
+      <Form method='post'>
+        <input
+          type='text'
+          name='username'
+          autoComplete='username'
+          placeholder='username'
+        />
+
         <br />
-        <label>
-          Password:
-          <input
-            type='password'
-            name='password'
-            value={loginData.password}
-            onChange={handleInputChange}
-            autoComplete='password'
-          />
-        </label>
+
+        <input
+          type='password'
+          name='password'
+          placeholder='password'
+          autoComplete='password'
+        />
         <br />
-        <button type='submit'>Login</button>
-      </form>
+        <button type='submit'>Log In</button>
+      </Form>
     </div>
   );
 }
